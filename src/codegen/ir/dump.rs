2c5ca4724e5e27877fbb034d591ff7de8f358949
John Hodge <tpg@mutabah.net>
2025-06-08 12:13:02 +0800

    IR dump - Tweak output to be cleared on order of operations with deref

--- src/codegen/ir/dump.rs
+++ src/codegen/ir/dump.rs
@@ -136,7 +136,7 @@ impl<'a> ::std::fmt::Display for F<'a, super::Value> {
         Value::ImplicitUnit => write!(f, "()"),
         Value::Local(local_index, wrapper_list) => write!(f, "{}{}", F(local_index), F(wrapper_list)),
         Value::Named(absolute_path, wrapper_list) => write!(f, "{}{}", absolute_path, F(wrapper_list)),
-        Value::Deref { ptr, wrappers } => write!(f, "*{}{}", F(ptr), F(wrappers)),
+        Value::Deref { ptr, wrappers } => write!(f, "(*{}){}", F(ptr), F(wrappers)),
         Value::StringLiteral(string_literal) => write!(f, "{:?}", string_literal),
         Value::IntegerLiteral(v) => write!(f, "{:#x}", v),
         Value::FunctionPointer(path, super::FunctionPointerTy::Function) => write!(f, "{}", path),
